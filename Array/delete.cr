b1665e0390dbf9b55ec0285db9d2eaa6cebc8143
tbpgr <tbpgr@tbpgr.jp>
2015-07-10 00:46:39 +0900

    :new: add: Array#delete

---
+++ Array/delete.cr
@@ -0,0 +1,12 @@
+ary = [1,2,3,4,5]
+pp ary
+pp ary.delete(2)
+pp ary
+pp ary.delete(2)
+pp ary
+
+# => ary = [1, 2, 3, 4, 5]
+# => ary.delete(2) = true
+# => ary = [1, 3, 4, 5]
+# => ary.delete(2) = false
+# => ary = [1, 3, 4, 5]
