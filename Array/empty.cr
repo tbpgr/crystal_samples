f5fbf4239117e5bc23c0242613bc88b92c83cf57
tbpgr <tbpgr@tbpgr.jp>
2015-07-16 00:29:55 +0900

    :new: add: Array#empty?

---
+++ Array/empty.cr
@@ -0,0 +1,9 @@
+pp Array(String).new.empty?
+pp [nil].empty?
+pp ['a'].empty?
+pp [1].empty?
+
+# => Array(String).new.empty? = true
+# => [nil].empty? = false
+# => ['a'].empty? = false
+# => [1].empty? = false
