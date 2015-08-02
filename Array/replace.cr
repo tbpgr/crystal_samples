0976a4962860cd0dffcbc847631f2d6fb6a973cc
tbpgr <tbpgr@tbpgr.jp>
2015-08-03 00:17:14 +0900

    :new: add: Array#replace

---
+++ Array/replace.cr
@@ -0,0 +1,8 @@
+hoges = ["hoge", "hige", "hage"]
+pp hoges
+pp hoges.replace(["ほげ", "ひげ", "はげ"])
+pp hoges
+
+# => hoges = ["hoge", "hige", "hage"]
+# => hoges.replace(["ほげ", "ひげ", "はげ"]) = ["ほげ", "ひげ", "はげ"]
+# => hoges = ["ほげ", "ひげ", "はげ"]
