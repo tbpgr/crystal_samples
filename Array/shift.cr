b5ba496abc85ad8457edbb606639dc37ab094f36
tbpgr <tbpgr@tbpgr.jp>
2015-08-09 01:37:09 +0900

    :new: add: Array#shift

---
+++ Array/shift.cr
@@ -0,0 +1,11 @@
+hoges = ["hoge", "hige", "hage"]
+pp hoges.shift
+pp hoges
+pp hoges.shift
+pp hoges
+
+# => hoges.shift = "hoge"
+# => hoges = ["hige", "hage"]
+# => hoges.shift = "hige"
+# => hoges = ["hage"]
+
