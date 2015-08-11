fb77131732b07fe6c9c1080e382f8d7e6b2ed543
tbpgr <tbpgr@tbpgr.jp>
2015-08-12 01:00:17 +0900

    :new: add: Array#size

---
+++ Array/size.cr
@@ -0,0 +1,11 @@
+hoges = %w(hoge hige hage)
+pp hoges.size
+pp hoges.length
+hoges.push("hege")
+pp hoges.size
+pp hoges.length
+
+# => hoges.size = 3
+# => hoges.length = 3
+# => hoges.size = 4
+# => hoges.length = 4
