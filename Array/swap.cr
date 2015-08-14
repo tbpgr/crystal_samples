ce2df18817f435332d02bc4bd46324b1b9950961
tbpgr <tbpgr@tbpgr.jp>
2015-08-15 01:43:02 +0900

    :new: add: Array#swap

---
+++ Array/swap.cr
@@ -0,0 +1,10 @@
+hoges = ["hoge", "hige", "hage"]
+pp hoges.swap(0,1)
+pp hoges
+pp hoges.swap(0,-1)
+pp hoges
+
+# => hoges.swap(0, 1) = ["hige", "hoge", "hage"]
+# => hoges = ["hige", "hoge", "hage"]
+# => hoges.swap(0, -1) = ["hage", "hoge", "hige"]
+# => hoges = ["hage", "hoge", "hige"]
