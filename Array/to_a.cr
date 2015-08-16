6116a35a7088d6694d547a80dcce9468793ac2a8
tbpgr <tbpgr@tbpgr.jp>
2015-08-17 01:40:46 +0900

    :new: add: Array#to_set

---
+++ Array/to_a.cr
@@ -0,0 +1,6 @@
+hoges = ["hoge", "hige", "hage", "hoge", "hige", "hage"]
+pp hoges
+pp hoges.to_set
+
+# => hoges = ["hoge", "hige", "hage", "hoge", "hige", "hage"]
+# => hoges.to_set = Set{"hoge", "hige", "hage"}
