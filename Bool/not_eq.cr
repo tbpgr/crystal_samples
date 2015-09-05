171bfd06bbcd0e9335af6169b70e1ac24bd8746d
tbpgr <tbpgr@tbpgr.jp>
2015-09-06 02:17:46 +0900

    :new: add: Bool#!=

---
+++ Bool/not_eq.cr
@@ -0,0 +1,11 @@
+pp !true
+pp !false
+pp true.!
+pp false.!
+
+[[true, true], [true, false], [false, true], [false, false]].each do |e|
+  a = e.first
+  b = e.last
+  puts "case a = #{a}, b = #{b}"
+  pp a != b
+end
