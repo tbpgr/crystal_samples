cc36b4b477fda6eda19bf8ef968eade9d579f9f3
tbpgr <tbpgr@tbpgr.jp>
2016-01-13 00:43:42 +0900

    :new: add: Enumerable#take

---
+++ Enumerable/take.cr
@@ -0,0 +1,18 @@
+class Fibonacci
+  include Enumerable(Int32)
+
+  def each
+    a, b = 0, 1
+    loop do
+      yield b
+      a, b = b, a + b
+    end
+  end
+end
+
+fib = Fibonacci.new
+puts fib.take(20)
+puts fib.take(10)
+
+# => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+# => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
