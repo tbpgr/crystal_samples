e9574bbe4d45c79989e89c7ffc695ffbc70ef884
tbpgr <tbpgr@tbpgr.jp>
2016-02-08 02:05:22 +0900

    :new: add: Float#milliseconds

---
+++ Float/milliseconds.cr
@@ -0,0 +1,7 @@
+pp 1.1.milliseconds
+pp 1.3.milliseconds
+pp 1.5.milliseconds
+
+# => 1.1.milliseconds = 00:00:00.0010000
+# => 1.3.milliseconds = 00:00:00.0010000
+# => 1.5.milliseconds = 00:00:00.0020000
