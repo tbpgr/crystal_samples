e4ddee98a988c8fb57396f1e400fc9462e3a7c9d
tbpgr <tbpgr@tbpgr.jp>
2016-03-20 02:32:40 +0900

    :new: add: Hash#to_s

---
+++ Hash/to_s.cr
@@ -0,0 +1,11 @@
+hash = {
+  key1: :value1,
+  key2: :value2,
+  hoge_key: :hoge_value
+}
+
+pp hash
+puts hash.to_s
+
+# => hash = {:key1 => :value1, :key2 => :value2, :hoge_key => :hoge_value}
+# => {:key1 => :value1, :key2 => :value2, :hoge_key => :hoge_value}
