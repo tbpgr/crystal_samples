df48fc41bc97280280afcf9a13645b1ff272aaf7
tbpgr <tbpgr@tbpgr.jp>
2016-04-09 01:22:46 +0900

    :new: add: Int#downto

---
+++ Int/down_to.cr
@@ -0,0 +1,5 @@
+pp 256.downto(254)
+pp 256.downto(250)
+
+# => 256.downto(254) = #<Int::DowntoIterator(Int32, Int32):0x80d1f18 @from=256, @to=254, @current=256>
+# => 256.downto(250) = #<Int::DowntoIterator(Int32, Int32):0x80d1f00 @from=256, @to=250, @current=256>
