34ebf72a36c3be54139efd02e902509177da4a57
tbpgr <tbpgr@tbpgr.jp>
2016-04-28 01:23:25 +0900

    :new: add: Int#to

---
+++ Int/to.cr
@@ -0,0 +1,12 @@
+3.to(5){|e|pp "#{e}times"}
+2.to(7){|e|pp "#{e}times"}
+
+# => "#{e}times" = "3times"
+# => "#{e}times" = "4times"
+# => "#{e}times" = "5times"
+# => "#{e}times" = "2times"
+# => "#{e}times" = "3times"
+# => "#{e}times" = "4times"
+# => "#{e}times" = "5times"
+# => "#{e}times" = "6times"
+# => "#{e}times" = "7times"
