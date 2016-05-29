ef1fdc170525a194052b7961d9f2ac747544b167
tbpgr <tbpgr@tbpgr.jp>
2016-05-30 00:31:05 +0900

    :new: add: Regex#+

---
+++ Regex/plus.cr
@@ -0,0 +1,10 @@
+regex = Regex.new("h.ge")
+regex =  regex + /ha.e/
+%w(hoge hige hage hane hase hise).map{|e|pp regex.match(e)}
+
+# => regex.match(e) = #<Regex::MatchData "hoge">
+# => regex.match(e) = #<Regex::MatchData "hige">
+# => regex.match(e) = #<Regex::MatchData "hage">
+# => regex.match(e) = #<Regex::MatchData "hane">
+# => regex.match(e) = #<Regex::MatchData "hase">
+# => regex.match(e) = nil
