0079457f46faef81bfd578abed58428ef17b300c
tbpgr <tbpgr@tbpgr.jp>
2016-06-12 03:14:38 +0900

    :new: add: SecureRandom.uuid

---
+++ SecureRandom/uuid.cr
@@ -0,0 +1,7 @@
+require "secure_random"
+
+pp SecureRandom.uuid
+pp SecureRandom.uuid
+
+# SecureRandom.uuid = "038fda63-7c60-4a7a-9fc1-26fd7765c5ab"
+# SecureRandom.uuid = "1bdb890d-22b7-404f-beee-e29df3de2b8a"
