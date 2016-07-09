a428bc9a53e746a0c985eef11370712ffef2fe85
tbpgr <tbpgr@tbpgr.jp>
2016-07-10 03:18:10 +0900

    :new: add: SimpleHash#each_key

--- SimpleHash/each_key.cr
+++ SimpleHash/each_key.cr
@@ -2,7 +2,7 @@ require "simple_hash"
 s = SimpleHash(Symbol, String).new
 s[:key1] = "value1"
 s[:key2] = "value2"
-s.each{|k,v|pp "#{k}:#{v}"}
+s.each_key{|e|pp e}
 
-# "#{k}:#{v}" # => "key1:value1"
-# "#{k}:#{v}" # => "key2:value2"
+# e # => :key1
+# e # => :key2
