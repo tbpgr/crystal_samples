7ae3ff81472e9ffdcc7a9f571649e33ba970ed70
tbpgr <tbpgr@tbpgr.jp>
2016-07-21 01:12:20 +0900

    add String#%

---
+++ String/percent.cr
@@ -0,0 +1,5 @@
+pp "My name is %s. I'm %d years old" % ["tanaka" , 23]
+pp "My name is %s. I'm %d years old" % ["suzuki" , 32]
+
+# "My name is %s. I'm %d years old" % ["tanaka", 23] # => "My name is tanaka. I'm 23 years old"
+# "My name is %s. I'm %d years old" % ["suzuki", 32] # => "My name is suzuki. I'm 32 years old"
