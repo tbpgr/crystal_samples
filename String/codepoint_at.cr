4cbfb92e4c998ccd7868e97eae7ef81b9a5f166b
tbpgr <tbpgr@tbpgr.jp>
2016-08-07 00:28:46 +0900

    String#codepoint_at

---
+++ String/codepoint_at.cr
@@ -0,0 +1,9 @@
+pp "hoge".codepoint_at(1)
+pp "hoge".codepoint_at(2)
+pp "ほげひげはげ".codepoint_at(1)
+pp "ほげひげはげ".codepoint_at(2)
+
+# "hoge".codepoint_at(1) # => 111
+# "hoge".codepoint_at(2) # => 103
+# "ほげひげはげ".codepoint_at(1) # => 12370
+# "ほげひげはげ".codepoint_at(2) # => 12402
