73f1ae619055cbc608c0246892cc529c99255b5b
tbpgr <tbpgr@tbpgr.jp>
2016-08-10 00:28:12 +0900

    Use named arguments

--- String/compare.cr
+++ String/compare.cr
@@ -1,12 +1,16 @@
 hoge = "hoge"
-pp hoge.compare("hoge", true)
-pp hoge.compare("hoge", false)
-pp hoge.compare("HOGE", true)
-pp hoge.compare("HOGE", false)
-pp hoge.compare("zzz", true)
+pp hoge.compare("hoge", case_insensitive: true)
+pp hoge.compare("hoge", case_insensitive: false)
+pp hoge.compare("hoge")
+pp hoge.compare("HOGE", case_insensitive: true)
+pp hoge.compare("HOGE", case_insensitive: false)
+pp hoge.compare("HOGE")
+pp hoge.compare("zzz", case_insensitive: true)
 
-# hoge.compare("hoge", true) # => 0
-# hoge.compare("hoge", false) # => 0
-# hoge.compare("HOGE", true) # => 0
-# hoge.compare("HOGE", false) # => 1
-# hoge.compare("zzz", true) # => -1
+# hoge.compare("hoge", case_insensitive: true) # => 0
+# hoge.compare("hoge", case_insensitive: false) # => 0
+# hoge.compare("hoge") # => 0
+# hoge.compare("HOGE", case_insensitive: true) # => 0
+# hoge.compare("HOGE", case_insensitive: false) # => 1
+# hoge.compare("HOGE") # => 1
+# hoge.compare("zzz", case_insensitive: true) # => -1
