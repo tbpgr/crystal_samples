aefde3a276b39ee7c3f7d076f4fbca4ca69e67f1
tbpgr <tbpgr@tbpgr.jp>
2016-08-24 02:39:42 +0900

    Add String#insert

---
+++ String/insert.cr
@@ -0,0 +1,5 @@
+pp "hogehigehage".insert(1, '*')       # char
+pp "hogehigehage".insert(1, "**")       # char
+
+# "hogehigehage".insert(1, '*') # => "h*ogehigehage"
+# "hogehigehage".insert(1, "**") # => "h**ogehigehage"
