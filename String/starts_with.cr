6df68fb62a197a093b7916febdba3e9b54d9e3c8
tbpgr <tbpgr@tbpgr.jp>
2016-09-04 01:10:01 +0900

    Add String#starts_with

---
+++ String/starts_with.cr
@@ -0,0 +1,9 @@
+pp "UUURYYY!".starts_with?('U')
+pp "UUURYYY!".starts_with?('V')
+pp "UUURYYY!".starts_with?("UUX")
+pp "UUURYYY!".starts_with?("UUU")
+
+# "UUURYYY!".starts_with?('U') # => true
+# "UUURYYY!".starts_with?('V') # => false
+# "UUURYYY!".starts_with?("UUX") # => false
+# "UUURYYY!".starts_with?("UUU") # => true
