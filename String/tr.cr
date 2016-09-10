a57e37de001b9938ef23cb2a9d0a0e83b757be71
tbpgr <tbpgr@tbpgr.jp>
2016-09-11 02:19:25 +0900

    Add String#tr

---
+++ String/tr.cr
@@ -0,0 +1,7 @@
+pp "hogehige".tr("hg", "+-")
+pp "hogehige".tr("h", "+")
+pp "hogehige".tr("h", "+-")
+
+# "hogehige".tr("hg", "+-") # => "+o-e+i-e"
+# "hogehige".tr("h", "+") # => "+oge+ige"
+# "hogehige".tr("h", "+-") # => "+oge+ige"
