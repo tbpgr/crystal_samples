7385cbbadc99c90d141aeb0050c91703d555c2a9
tbpgr <tbpgr@tbpgr.jp>
2016-10-17 01:56:14 +0900

    Add Time#add_ticks

---
+++ Time/add_ticks.cr
@@ -0,0 +1,5 @@
+pp Time.now.add_ticks(100_000_000_i64)
+pp Time.now.add_ticks(200_000_000_i64)
+
+# Time.now.add_ticks(100000000_i64) # => 2016-10-16 16:55:48 +0000
+# Time.now.add_ticks(200000000_i64) # => 2016-10-16 16:55:58 +0000
