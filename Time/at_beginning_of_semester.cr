6b8b041dbd9c890bd7b5159fe1c93624a17c4860
tbpgr <tbpgr@tbpgr.jp>
2016-10-23 02:02:52 +0900

    Add Time/at_beginning_of_semester

---
+++ Time/at_beginning_of_semester.cr
@@ -0,0 +1,5 @@
+pp Time.new(2016, 2, 3, 4, 5, 1).at_beginning_of_semester
+pp Time.new(2016, 8, 3, 4, 5, 1).at_beginning_of_semester
+
+# (Time.new(2016, 2, 3, 4, 5, 1)).at_beginning_of_semester # => 2016-01-01 00:00:00
+# (Time.new(2016, 8, 3, 4, 5, 1)).at_beginning_of_semester # => 2016-07-01 00:00:00
