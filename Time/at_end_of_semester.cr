a28886b60faa3e3e3a603916e366d8e6d9bdacff
tbpgr <tbpgr@tbpgr.jp>
2016-10-31 03:53:04 +0900

    Add Time#at_end_of_semester

---
+++ Time/at_end_of_semester.cr
@@ -0,0 +1,5 @@
+pp Time.new(2016, 1, 2, 3, 4, 5)
+pp Time.new(2016, 1, 2, 3, 4, 5).at_end_of_semester
+
+# Time.new(2016, 1, 2, 3, 4, 5) # => 2016-01-02 03:04:05
+# (Time.new(2016, 1, 2, 3, 4, 5)).at_end_of_semester # => 2016-06-30 23:59:59
