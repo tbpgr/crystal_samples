e78eeaaca8225f900ad0358a88525b714425e00c
tbpgr <tbpgr@tbpgr.jp>
2016-12-11 00:11:40 +0900

    Add Tuple#reverse

---
+++ Tuple/reverse.cr
@@ -0,0 +1,5 @@
+pp (["key", "value", 1, 2])
+pp (["key", "value", 1, 2]).reverse
+
+# ["key", "value", 1, 2] # => ["key", "value", 1, 2]
+# ["key", "value", 1, 2].reverse # => [2, 1, "value", "key"]
