64fc8194d8550a0df2f5745581173e816e231935
tbpgr <tbpgr@tbpgr.jp>
2016-12-24 21:41:48 +0900

    Add URI#full_path

---
+++ URI/full_path.cr
@@ -0,0 +1,5 @@
+require "uri"
+uri = URI.new(scheme = "https", host = "example.com", port = 3000, path = "/users", query = "q=tanaka", user = "sato", password = "sato9999")
+pp uri.full_path
+
+# uri.full_path # => "/users?q=tanaka"
