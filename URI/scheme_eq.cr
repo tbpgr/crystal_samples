cc560e3503c663355ff4e2a9b95808335ad22466
tbpgr <tbpgr@tbpgr.jp>
2017-01-09 01:24:17 +0900

    Add URI#scheme=

--- URI/scheme_eq.cr
+++ URI/scheme_eq.cr
@@ -2,8 +2,11 @@ require "uri"
 uri = URI.new(scheme = "https", host = "example.com", port = 3000, path = "/users", query = "q=tanaka", user = "sato", password = "sato9999")
 pp uri
 pp uri.scheme
+pp uri.scheme = "updated"
+pp uri
+pp uri.scheme
 
-# uri # => #<URI:0x9a5ff60
+# uri # => #<URI:0x85f5f60
 #           @fragment=nil,
 #           @host="example.com",
 #           @opaque=nil,
@@ -14,3 +17,15 @@ pp uri.scheme
 #           @scheme="https",
 #           @user="sato">
 # uri.scheme # => "https"
+# uri.scheme = "updated" # => "updated"
+# uri # => #<URI:0x85f5f60
+#           @fragment=nil,
+#           @host="example.com",
+#           @opaque=nil,
+#           @password="sato9999",
+#           @path="/users",
+#           @port=3000,
+#           @query="q=tanaka",
+#           @scheme="updated",
+#           @user="sato">
+# uri.scheme # => "updated"
